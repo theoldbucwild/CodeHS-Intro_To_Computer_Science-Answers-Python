0d0e0f830eae36dd7070502d5ac9af11071b4710
theoldbucwild <34425406+theoldbucwild@users.noreply.github.com>
2020-05-22 07:36:58 -0400

    Update 7.4.8 Count Occurences
    The previous code did not work and I wanted to update it for anyone who needed help

--- CodeHs/5.Strings/4. Strings And For Loops/7.4.8 Count Occurrences.py
+++ CodeHs/5.Strings/4. Strings And For Loops/7.4.8 Count Occurrences.py
@@ -1,8 +1,10 @@
 def count_occurrences(a,b):
     counter = 0
-    for checker in a:
-        if(checker == b):
+    for check in a:
+        if(check == b):
             counter+=1
     return counter
+word = input("Enter a word:")
+letter = input("Enter a letter:")
 
-print count_occurrences("haaa","a")
+print count_occurrences(word, letter)
